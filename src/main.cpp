f525cd11b761a74cb9e2aa79c00d497daa007e1d
Douglas Ward <30300064+dougimus@users.noreply.github.com>
2022-09-30 11:25:45 -0400

    Update main.cpp
    added statement of authorship

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <Arduino.h>
 
+// I, Douglas Ward, 000333587, certify that all code submitted is my own work; 
+// that I have not copied it from any other source. I also certify that I have not allowed my work to be copied by others.
+
 boolean bState;
 boolean bLastState;
 
